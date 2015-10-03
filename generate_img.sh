0f324e4fce65c0cc1749a3dc137f05daaa275c5d
Jens Maus <mail@jens-maus.de>
2015-10-04 01:10:13 +0200

    added 'generate_img.sh' script to generate addon package.

---
+++ generate_img.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+mkdir -p tmp
+cp -a account.conf tmp/
+cp -a addon tmp/
+cp -a ccu1 tmp/
+cp -a ccu2 tmp/
+cp -a ccu-rpi2 tmp/
+cp -a email tmp/
+cp -a mails tmp/
+cp -a msmtp.conf tmp/
+cp -a update_script tmp/
+cp -a userscript.tcl tmp/
+cp -a www tmp/
+cd tmp
+tar -czvf ../email-1.6.1.tar.gz *
+cd ..
+rm -rf tmp

c0607989a335a9da896e4a91cfedeb1b3579ab60
Jens Maus <mail@jens-maus.de>
2016-06-09 11:41:50 +0200

    simplified the rc.d and update_script scripts by adding the 'update_addon' tool for adding the Addon Button to the right place. We also use the /tmp directory rather than having to maintain an own tmp directory within our addon directory which might cause SD card corruption upon crashes.

--- generate_img.sh
+++ generate_img.sh
@@ -4,8 +4,8 @@ cp -a account.conf tmp/
 cp -a addon tmp/
 cp -a ccu1 tmp/
 cp -a ccu2 tmp/
-cp -a ccupi tmp/
-cp -a email tmp/
+cp -a ccurm tmp/
+cp -a rc.d tmp/
 cp -a mails tmp/
 cp -a msmtp.conf tmp/
 cp -a update_script tmp/
@@ -13,6 +13,6 @@ cp -a userscript.tcl tmp/
 cp -a www tmp/
 cp -a VERSION tmp/addon/
 cd tmp
-tar --owner=root --group=root -czvf ../email-$(cat ../VERSION).tar.gz *
+tar --owner=root --group=root --exclude=.DS_Store -czvf ../email-$(cat ../VERSION).tar.gz *
 cd ..
 rm -rf tmp
